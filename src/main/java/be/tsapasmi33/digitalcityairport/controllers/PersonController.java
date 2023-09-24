549d2531a1d1828c412a172ef37151b73edbf798
tsapasMi33 <tsapas_mi@hotmail.com>
2023-09-24 15:47:35 +0200

    ++

--- src/main/java/be/tsapasmi33/digitalcityairport/controllers/PersonController.java
+++ src/main/java/be/tsapasmi33/digitalcityairport/controllers/PersonController.java
@@ -2,24 +2,30 @@ package be.tsapasmi33.digitalcityairport.controllers;
 
 import be.tsapasmi33.digitalcityairport.models.entities.Person;
 import be.tsapasmi33.digitalcityairport.services.PersonService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
+import lombok.AllArgsConstructor;
+import org.springframework.data.web.JsonPath;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/people")
 public class PersonController {
 
     PersonService personService;
-
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+    ObjectMapper objectMapper;
 
     @GetMapping("/all")
     public ResponseEntity<List<Person>> getAll(){
-        System.out.println("ok");
         return ResponseEntity.ok()
                 .body(personService.getPeople());
     }
@@ -42,7 +48,45 @@ public class PersonController {
 
     @PostMapping("/add")
     public ResponseEntity<Person> addPerson(@RequestBody Person person) {
-
+        personService.addPerson(person);
+        return ResponseEntity.created(URI.create("/people/"+person.getId()))
+                .build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletePerson(@PathVariable String id) {
+        personService.deletePerson(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> modifyPerson(@PathVariable String id, @RequestBody Person person) {
+        return ResponseEntity
+                .accepted()
+                .body(personService.updatePerson(id, person));
+    }
+
+    @PatchMapping(value = "/{id}/patch", consumes = "application/json-patch+json")
+    public ResponseEntity<Person> updateFidelity(@PathVariable String id, @RequestBody JsonPatch patch) {
+        try {
+            Person person = personService.getPerson(id);
+            Person patchedPerson = applyPatchToPerson(patch, person);
+            personService.updatePerson(id, patchedPerson);
+            return ResponseEntity.ok()
+                    .body(patchedPerson);
+        } catch (JsonPatchException | JsonProcessingException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    private Person applyPatchToPerson(JsonPatch patch, Person targetPerson) throws JsonPatchException, JsonProcessingException {
+        JsonNode patched = patch.apply(objectMapper.convertValue(targetPerson, JsonNode.class));
+        return objectMapper.treeToValue(patched, Person.class);
+    }
+
+//    {
+//        "op":"replace",
+//            "path":"/status",
+//            "value":"GOLD"
+//    }
 }
